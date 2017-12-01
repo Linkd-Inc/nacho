02cb3509c5d96a9a7d5ea8de7f6bf3971325e764
Corbin <corbin@wireflare.com>
2017-12-01 12:22:12 -0800

    Initial commit. Creates a working framework view

---
+++ README.md
@@ -0,0 +1,43 @@
+# Nacho - A minimal, highly customizable flexbox framework
+
+[![npm version](https://badge.fury.io/js/nacho.svg)](https://badge.fury.io/js/nacho)
+[![Build Status](https://travis-ci.org/WireFlare/nacho.svg?branch=master)](https://travis-ci.org/WireFlare/nacho)
+
+
+## Installation
+
+To add nacho to your web page, simply paste this code into the `head` of your document,
+```html
+<link rel="stylesheet" href="https://unpkg.com/nacho/dist/nacho.min.css">
+<!-- or -->
+<link rel="stylesheet" href="https://unpkg.com/nacho/dist/nacho.css">
+```
+or you can install nacho using a package manager like [npm](https://npmjs.com/package/nacho)
+```bash
+$ npm install nacho
+```
+If you want you can still download nacho [here](https://github.com/WireFlare/nacho/releases/download/v0.0.1/nacho.min.css).
+
+### Example:
+
+```html
+<div>
+  <div class="row">
+    <!-- For column width, use the number / 12 -->
+    <div class="col-xs-3"></div>
+    <div class="col-sm-3"></div>
+    <div class="col-md-3"></div>
+    <div class="col-lg-3"></div>
+  </div>
+  <div class="col">
+    <!-- The col parent class makes items stack vertically -->
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</div>
+```
+
+## License
+
+Copyright (c) 2017 Wireflare. Licensed under the MIT License (MIT)
---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+const runSequence = require('run-sequence');
+const cssbeautify = require('gulp-cssbeautify');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass:compile', function () {
+  return gulp.src('./src/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({
+      level: {
+        1: {
+          all: true
+        },
+        2: {
+          all: true,
+        }
+      }
+    })) // This moight seem counter intuitive at first, that we first uglify then beautify, but the uglification drastically reduces file size by merging col-1-of-4, col-2-of-8 and col-3-of-12 ito one same rule. The beautyfication makes the code readable. This reduces the non-minified file size by ~200 lines.
+    .pipe(cssbeautify({
+      indent: '  ',
+      autosemicolon: true
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('css:minify', function() {
+  return gulp.src('dist/nacho.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename('nacho.min.css'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', function () {
+  runSequence('sass:compile', 'css:minify');
+});
+
+gulp.task('watch', ['build'], function () {
+  gulp.watch('./src/**/*.scss', function () {
+    runSequence('sass:compile', 'css:minify');
+  });
+});
+
+gulp.task('default', ['build']);
